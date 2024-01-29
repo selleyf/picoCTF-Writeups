ae04f00ab228b774bb5b49cf7a97c8f6011196c3
Fanni Sélley <125468063+selleyf@users.noreply.github.com>
2024-01-29 15:38:31 +0100

    Update xtra_solution.py w/ itertools

--- XtraORdinary/xtra_solution.py
+++ XtraORdinary/xtra_solution.py
@@ -1,3 +1,5 @@
+import itertools
+
 random_strs = [
     b'my encryption method',
     b'is absolutely impenetrable',
@@ -9,19 +11,13 @@ random_strs = [
 hex_string = "57657535570c1e1c612b3468106a18492140662d2f5967442a2960684d28017931617b1f3637"  
 byte_string = bytes.fromhex(hex_string)
 
-# by trial-end error we see that the encryption was done with three random strings
-for key1 in random_strs:
-    for key2 in random_strs:
-        for key3 in random_strs:
-            #for key4 in random_strs:
-                #for key5 in random_strs:
-                    ctxt = encrypt(byte_string, key1)
-                    ctxt = encrypt(ctxt, key2)
-                    ctxt = encrypt(ctxt, key3)
-                    #ctxt = encrypt(ctxt, key4)
-                    #ctxt = encrypt(ctxt, key5)
-                    key = encrypt(b'picoCTF',ctxt[:7])
-                    print(key.decode("ASCII"))
-                    ctxt = encrypt(ctxt, key)
-                    print(ctxt.decode("ASCII")) 
+# by trial-end error we see that the encryption was done with 3 random strings
+for combination in itertools.combinations(random_strs, 3):
+    ctxt = byte_string
+    for random_str in combination:
+        ctxt = encrypt(ctxt, random_str)
+    key = encrypt(b'picoCTF',ctxt[:7])
+    print(key.decode("ASCII")) 
+    ctxt = encrypt(ctxt, key)
+    print(ctxt.decode("ASCII"))
 
